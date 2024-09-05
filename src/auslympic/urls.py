17b6568798ace54de425d08e133bd0427f611ca5
Himprakash Deka <himprakashd@gmail.com>
2024-09-05 09:37:16 +0530

    update

--- src/auslympic/urls.py
+++ src/auslympic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, SportView, LeaderBoard
+from .views import Home, SportView, LeaderBoard, NoticeView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('leaderboards/', LeaderBoard.as_view(), name='leaderboard'),
+    path('leaderboard/', LeaderBoard.as_view(), name='leaderboard'),
+    path('notices/', NoticeView.as_view(), name='notice'),
     path('<int:pk>/', SportView.as_view(), name='sport')
 ]
