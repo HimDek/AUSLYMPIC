11069991319789ea2f1b43e792aed311d2b1d9fe
Himprakash Deka <himprakashd@gmail.com>
2024-09-05 09:38:13 +0530

    update

--- src/auslympic/urls.py
+++ src/auslympic/urls.py
@@ -4,6 +4,6 @@ from .views import Home, SportView, LeaderBoard, NoticeView
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('leaderboard/', LeaderBoard.as_view(), name='leaderboard'),
-    path('notices/', NoticeView.as_view(), name='notice'),
+    path('notices/', NoticeView.as_view(), name='notices'),
     path('<int:pk>/', SportView.as_view(), name='sport')
 ]
