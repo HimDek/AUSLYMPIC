854238a0ce36055958c2a126ac8daf1c2c74e77e
Himprakash Deka <himprakashd@gmail.com>
2024-09-06 20:55:21 +0530

    domain

--- src/main/settings_vercel.py
+++ src/main/settings_vercel.py
@@ -4,6 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = [".vercel.app", "now.sh"]
+ALLOWED_HOSTS = [".auslympic.info", ".vercel.app", "now.sh"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
