32506cda3c0ca6fdb56c2a7496da87a7d8789a1b
Himprakash Deka <himprakashd@gmail.com>
2024-09-07 20:35:33 +0530

    update

--- serve.sh
+++ serve.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+python -m venv env
 source env/bin/activate
+
 cd src
-npm i
+
+npm install
 npm run scss
+
 python manage.py migrate
 python manage.py collectstatic --no-input
+
 gunicorn --workers 3 --bind 127.0.0.1:8000 main.wsgi
--- src/main/settings.py
+++ src/main/settings.py
@@ -4,14 +4,8 @@ from .settings_base import *
 if os.getenv('PRODUCTION_SECRET_KEY'):
     from .settings_production import *
 
-if os.getenv('POSTGRES_DATABASE'):
-    from .settings_postgresql import *
-
 if os.getenv('RDS_HOSTNAME'):
     from .settings_rds import *
-
-if os.getenv('VERCEL'):
-    from .settings_vercel import *
 
 if os.getenv('AWS'):
     from .settings_aws import *
