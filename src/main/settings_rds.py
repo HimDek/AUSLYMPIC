59a7d5be67772e630b5e9ca867cb5e57f0152c84
Himprakash Deka <himprakashd@gmail.com>
2024-09-07 16:22:45 +0530

    update

---
+++ src/main/settings_rds.py
@@ -0,0 +1,15 @@
+import os
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
