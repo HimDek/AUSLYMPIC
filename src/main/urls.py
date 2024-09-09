b0fda7be0104feace8a56867db933a27ec3502aa
Himprakash Deka <himprakashd@gmail.com>
2024-09-09 16:57:39 +0530

    robots.txt

--- src/main/urls.py
+++ src/main/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('2024-admin-do-not-make-this-url-public/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('', include('auslympic.urls'))
 ]
